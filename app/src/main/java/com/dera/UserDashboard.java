3ba454193cc87d4317e2ba49e70101a30be3faf5
Dilate-byte <dikshaghimire565@gmail.com>
2023-05-01 12:21:19 +0545

    four new fragment are added

---
+++ app/src/main/java/com/dera/UserDashboard.java
@@ -0,0 +1,75 @@
+package com.dera;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.google.android.material.button.MaterialButton;
+
+public class UserDashboard extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard);
+        ImageView home = findViewById(R.id.homeIV);
+        ImageView booking = findViewById(R.id.bookingIV);
+        ImageView  history = findViewById(R.id.historyIV);
+        ImageView  profile = findViewById(R.id.profileIV);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new UserHome();
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragmentlayout, fragment);
+                transaction.commit();
+            }
+        });
+        booking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new UserBooking();
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragmentlayout, fragment);
+                transaction.commit();
+            }
+        });
+        history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new UserHistory();
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragmentlayout, fragment);
+                transaction.commit();
+            }
+        });
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new UserProfile();
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragmentlayout, fragment);
+                transaction.commit();
+            }
+        });
+    }
+
+}
