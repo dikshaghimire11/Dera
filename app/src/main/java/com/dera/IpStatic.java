6282de738aa1d66fe8a4e3ed522970bc58ae71c6
Dilate-byte <dikshaghimire565@gmail.com>
2023-06-16 20:12:40 +0545

    commiting Ipstatic

--- app/src/main/java/com/dera/IpStatic.java
+++ app/src/main/java/com/dera/IpStatic.java
@@ -3,6 +3,6 @@ package com.dera;
 public class IpStatic {
 
     public static class IpAddress{
-        public static String ip="192.168.1.9";
+        public static String ip="192.168.6.223";
     }
 }
